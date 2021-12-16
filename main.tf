991320f27beef9889a9c469578d8719e026482ca
Jose Rivera <jrivera@newrelic.com>
2021-12-16 15:16:10 -0800

    Update all endpoints

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ provider "aws" {
   s3_force_path_style         = true
   endpoints {
     s3 = "http://localhost:4566"
+    sqs = "http://localhost:4566"
+    sns = "http://localhost:4566"
   }
 }
 
